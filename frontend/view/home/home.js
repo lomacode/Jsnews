3c688dccdc510945fdda86a19a96c2d3d272a914
Matthieu Lombardia <85737628+matthieu-lombardia@users.noreply.github.com>
2022-03-03 14:21:15 +0100

    css reponsive MQ 490px

--- frontend/view/home/home.js
+++ frontend/view/home/home.js
@@ -1,3 +1,9 @@
 /**
  * Gère l'affichage et les interactions de la page d'accueil
  */
+
+fetch("http://localhost:4000/api/article")
+.then( data) => data.json()
+.then( jsonListArticle = > {
+  console.log(jsonListArticle);
+});
